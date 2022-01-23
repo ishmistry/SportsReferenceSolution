e8893cd5bd068bb9e9795b42df1ed5c96fcf7d8a
ishmistry <imistry@purdue.edu>
2022-01-23 16:58:55 -0500

    general read and parse structure created

--- src/SportsReferenceSolution.java
+++ src/SportsReferenceSolution.java
@@ -1,8 +1,28 @@
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
 public class SportsReferenceSolution {
 
-
+    LinkedList<LinkedList<String>> wl;
 
     public static void main(String[] args) {
+        JSONParser parser = new JSONParser(); // creating parser to read thru json file
+        try {
+            JSONArray array = (JSONArray) parser.parse(new FileReader("winloss.json")); // reading json file
 
+            for (Object o : array) {
+                JSONObject team = (JSONObject) o;
+
+            }
+
+        } catch (FileNotFoundException e) { // exceptions in the case invalid file is provided
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
